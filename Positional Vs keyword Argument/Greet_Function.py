4101b052cda3413bd5ebf5082d85d63313b3ed57
shivamku25 <shivam.25.kumr@gmail.com>
2025-06-24 23:44:44 +0530

    Caesar Cipher program The Code of encryption or decryption

---
+++ Positional Vs keyword Argument/Greet_Function.py
@@ -0,0 +1,18 @@
+# def greet_with_name(name):
+#     print(f"Hello,{name}")
+#     print(f"How do you do ,{name}")
+#
+# greet_with_name("jack Bauer")
+#
+# function with more than one input
+
+#def greet_with(name, location):
+
+# 1 Usage
+
+def greet_with(name, location):
+    print(f"Hello,{name}")
+    print(f"What is in like in location, {location}")
+
+#greet_with(name = "jack Bauer", location = "Bangalore")
+greet_with(location = "Bangalore", name =  "Jack bauer")
