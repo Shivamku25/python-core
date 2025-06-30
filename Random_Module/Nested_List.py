d16a666f54e3a2329bfcdbb64974c2ccc37c2e0a
shivamku25 <shivam.25.kumr@gmail.com>
2025-06-30 13:34:54 +0530

    Nested list

---
+++ Random_Module/Nested_List.py
@@ -0,0 +1,5 @@
+fruits = ["cherry","apple","mango","orange","strawberries"]
+vegetables = ["carrot","broccoli","spinach","potato","tomato"]
+nested_list = [fruits, vegetables]
+#print(nested_list)
+print(len(nested_list))
