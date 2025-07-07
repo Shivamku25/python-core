6cc0666f9e4d1b7662dd54f1574f9a01dbbe9d87
shivamku25 <shivam.25.kumr@gmail.com>
2025-07-07 20:05:09 +0530

    Type checking error

---
+++ Type_Checking_Error.py
@@ -0,0 +1,7 @@
+name_of_the_user = input("Enter Your Name\n")
+length_of_name = len("name-of_the_user\n")
+
+print(type("Number of letter of your name :"))#str
+print(type(length_of_name))#int
+
+print("Number of letter of your name : " + str(length_of_name))
