e6ba46e0f23ac29cdd4070acfad83cd8b6c5e859
shivamku25 <shivam.25.kumr@gmail.com>
2025-07-07 20:09:31 +0530

    Defining All data type

---
+++ All_Data_Type.py
@@ -0,0 +1,16 @@
+#subscripting
+#String
+print("Welcome to the bangalore"[23])
+
+#Integer
+print(123+456)
+
+#Large Integer
+print(123_4556_789)
+
+#Float (Floating point number)
+print(3.4567)
+
+#Boolean
+print(True)
+print(False)
