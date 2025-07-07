dd32a9420046dfa167cbd5e5912475e5d02313f6
shivamku25 <shivam.25.kumr@gmail.com>
2025-07-07 20:06:11 +0530

    Nesting Dictionary

---
+++ Dictionaries Nesting and the Secrut Auction/Task1.py
@@ -0,0 +1,20 @@
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin"
+
+}
+# Nested list in dictionary
+
+travel_log = {
+    "France": ["Paris", "Lille", "Dijon"],
+    "Germany": ["Stuttgart", "Berlin"],
+
+}
+
+#print Lille
+print(travel_log["France"][0])
+
+nested_list = ["A", "B", ["C", "D"]]
+# print D
+
+print(nested_list[2][1])
